ec023f7053ca0f3b543ccb643ae7bfa686a9aed8
galya-cmd <galya1985@bk.ru>
2023-02-02 22:31:17 +0300

    изменены картинки на svg

--- welbex/src/Header.js
+++ welbex/src/Header.js
@@ -1,6 +1,6 @@
 import './Header.css';
 import React from 'react';
-import logo from './assets/img/logo.svg';
+import logo from './assets/img/logo_welbex.svg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTelegram } from '@fortawesome/free-brands-svg-icons';
@@ -49,7 +49,6 @@ import {faHome} from '@fortawesome/free-solid-svg-icons';
                 <li className='header__item'><a href="tel:+7 555 555-55-55" className='footer__link header__link'>+7 555-55-55</a>
                 </li>
                     <li className='header__item-contacts'><a href="#"><FontAwesomeIcon icon={faTelegram} className='header__link icon'/></a></li>
-                    {/* <li> <a className='header__link' href='#'><FontAwesomeIcon icon={faPhone} /></a></li> */}
                     <li className='header__item-contacts'> <a className='header__link icon' href='#'><FontAwesomeIcon icon={faHome} /></a></li>
                     <li className='header__item-contacts'> <a className='header__link icon' href='#'><FontAwesomeIcon icon={faWhatsapp} /></a></li>
                 </ul>
