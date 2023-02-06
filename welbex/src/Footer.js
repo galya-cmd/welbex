b56fb4149967c3887e8a00ffc254309a3e4b4622
galya-cmd <galya1985@bk.ru>
2023-02-06 14:17:34 +0300

    изменила ссылки компонента footer

--- welbex/src/Footer.js
+++ welbex/src/Footer.js
@@ -1,5 +1,6 @@
 import './Footer.css';
 import React from "react";
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTelegram } from '@fortawesome/free-brands-svg-icons';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
@@ -20,18 +21,18 @@ function Footer (){
         <h4 className='Footer__tittle'>Меню</h4>  
         <div className='footer__container-li'> 
             <div className='footer__container-margin'>
-        <li className='footer__item'><a href='#' className='footer__link'>Расчёт стоимости</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Услуги</a></li>
-        <li className='footer__item' ><a href='#' className='footer__link'>Виджеты</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Интеграции</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Наши клиенты</a></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Расчёт стоимости</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Услуги</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Виджеты</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Интеграции</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Наши клиенты</Link></li>
         </div>
         <div className='footer__container-margin'>
-        <li className='footer__item'><a href='#' className='footer__link'>Кейсы</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Благодарственные письма</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Сертификаты</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Блог на Youtube</a></li>
-        <li className='footer__item'><a href='#' className='footer__link'>Вопрос / Ответ</a></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Кейсы</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Благодарственные письма</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Сертификаты</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Блог на Youtube</Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link'>Вопрос / Ответ</Link></li>
         </div>
         </div> 
         </ul>    
@@ -39,19 +40,19 @@ function Footer (){
             <div className='four'>
             <ul className='four__ul'> 
                 <h4 className='Footer__tittle'>Контакты</h4>
-        <li className='footer__item'><a href="tel:+7 555 555-55-55" className='footer__link'>+7 555 555-55-55</a>
-</li>
-<div className='footer__icon-list'>
-        <li className='footer__item'><a href='#' className='footer__link icon-list'><FontAwesomeIcon icon={faTelegram} /></a></li>
-        <li className='footer__item'><a href='#' className='footer__link icon-list'><FontAwesomeIcon icon={faHome} /></a></li>
-        <li className='footer__item'><a href='#' className='footer__link icon-list'><FontAwesomeIcon icon={faWhatsapp} /></a></li>
+        <li className='footer__item'><Link to="tel:+7 555 555-55-55" className='footer__link'>+7 555 555-55-55</Link>
+        </li>
+        <div className='footer__icon-list'>
+        <li className='footer__item'><Link to='#!' className='footer__link icon-list'><FontAwesomeIcon icon={faTelegram} /></Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link icon-list'><FontAwesomeIcon icon={faHome} /></Link></li>
+        <li className='footer__item'><Link to='#!' className='footer__link icon-list'><FontAwesomeIcon icon={faWhatsapp} /></Link></li>
         </div>
         <li className='footer__item'><p>Москва, Путевой проезд 3с1, к 902</p></li>
             </ul>
-<div className='footer__end'>
+        <div className='footer__end'>
     <div className='footer__color'>©WELBEX 2022. Все права защищены.</div>
     <div className='footer__color'>Политика конфиденциальности</div>
-</div>
+        </div>
             </div>
         </div>
     )
